f70d9c1ee4392c64994c1c7e9faf3ab437ecbdb6
aefehr <fehr.allie@gmail.com>
2024-05-16 18:46:08 -0400

    feat: Add social links

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './NavBar.css';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 function Navbar() {
     return (
         <div className="navbar">
             <a href="#home">Allie Fehr</a>
             <div className="navbar-links">
-                <a href="#about">About me</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact me</a>
+                <a href="#about"><b>About</b></a>
+                <a href="#projects"><b>Projects</b></a>
+                <a href="#contact"><b>Contact</b></a>
+            </div>
+            <div className="social-links">
+                <a href="https://linkedin.com/in/allie-fehr/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                <a href="https://github.com/aefehr" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
             </div>
         </div>
     );
