69e4de307b5a63a47994728d9af17afb4ec8d5f2
aefehr <fehr.allie@gmail.com>
2025-01-05 17:10:30 -0500

    Create third donut shape

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import GrainEffect from './components/GrainEffect';
 import IntroSection from './components/IntroSection';
-import PositionableSphere from './components/PostionableSphere';
 import DonutComponentOne from './components/DonutComponentOne';
 import DonutComponentTwo from './components/DonutComponentTwo';
+import DonutComponentThree from './components/DonutComponentThree';
 
 const App = () => (
   <div style={{ position: "relative", width: "100vw", height: "100vh", overflow: "hidden" }}>
     <div className="scene-container" style={{ position: "relative", width: "100%", height: "100%" }}>
       <IntroSection />
-      
-      <PositionableSphere position="top-left" size="large" color="#FFB3C2" />
       
       <DonutComponentOne 
         position="bottom-right"
@@ -24,6 +22,12 @@ const App = () => (
         color="#FFB3C2"
       />
     </div>
+
+    <DonutComponentThree 
+      position="bottom-left"
+      size="large"
+      color="#A7C7E7" // Using a blue-ish color like in the image, but you can change this
+    />
     
     {/* Grain effect overlay */}
     <GrainEffect />
