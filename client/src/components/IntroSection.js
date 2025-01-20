027bd90ec0a2a532786c8c0607231333e0e539e9
aefehr <fehr.allie@gmail.com>
2025-01-20 13:42:31 -0500

    Change intro fonts

--- client/src/components/IntroSection.js
+++ client/src/components/IntroSection.js
@@ -2,27 +2,28 @@ import React from "react";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import "./IntroSection.css"; 
 
-function IntroSection() {
+const IntroSection = () => {
   return (
     <section className="intro-section">
-      <h1 className="intro-heading outline-text">
-        HI, MY NAME IS <span className="name-highlight">ALLIE FEHR</span>
+      <h1 className="intro-heading">
+        <span className="outline-text">HI<span class="rounded">,</span> MY NAME IS </span>
+        <span className="name-highlight"> ALLIE</span>
       </h1>
       <h2 className="intro-subheading">
-        I am a senior at the University of Pennsylvania, data structures and algorithms tutor, &amp; previous software engineer intern at Yahoo 
+        I'm a senior at the University of Pennsylvania, data structures and algorithms tutor, &amp; previous software engineer intern at Yahoo 
       </h2>
       <div className="intro-links">
         <a href="/projects" className="link">
           <ArrowForwardIcon className="arrow" />
-          <span className="text">projects</span>
+          <span className="text">see my projects</span>
         </a>
         <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="link">
           <ArrowForwardIcon className="arrow" />
-          <span className="text">resume</span>
+          <span className="text">more about me</span>
         </a>
       </div>
     </section>
   );
-}
+};
 
 export default IntroSection;
