1c6e26b8438a01fd3f1100180101d8471753606a
aefehr <fehr.allie@gmail.com>
2025-02-04 21:52:00 -0500

    Add profile picture

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,33 +1,40 @@
 import React from "react";
 import "./About.css";
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import profileImg from '../assets/images/allie_profile_pic.png'
 
 const About = () => (
-  <div className="about-container">
-    {/* Left Section */}
-    <div className="about-left">
-      <h1 className="about-title">ABOUT</h1>
-      <p className="about-description">
-        Hey, my name is <b>Charles Bruyerre</b> and I use Sharlee as my nickname
-        across social media. I’m a graphic designer, UX/UI designer & front-end
-        web developer from France. I’m also passionate about pop music and make
-        portraits and universes around what I listen to. I’m always curious to
-        learn more when it comes to new technologies and creative coding.
-      </p>
-      <a
-        href="/path-to-your-resume.pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="resume-link"
-      >
-        <ArrowDownwardIcon className="resume-icon" />
-        resume
-      </a>
-    </div>
+  <div className="about-page">
+    <div className="about-container">
+      {/* Left Content */}
+      <div className="about-content">
+        <h1 className="about-title">ABOUT</h1>
+        <div className="underline"></div>
+        <p className="about-description">
+          Hey, my name is <b>Allie Fehr</b>. I'm a senior at the University of Pennsylvania
+          studying Computer Science and Cognitive Science (concentrating in computation and cognition).
+          I’m also passionate about language learning and work part-time tutoring English to non-native speakers.
+        </p>
+        <a
+          href="/path-to-your-resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="resume-link"
+        >
+          <ArrowDownwardIcon className="resume-icon" />
+          resume
+        </a>
+      </div>
 
-    {/* Right Section */}
-    <div className="about-right"></div>
+      {/* Right Image */}
+      <div className="about-image">
+        <img src={profileImg} alt="Allie Fehr" />
+      </div>
+    </div>
   </div>
 );
 
 export default About;
+
+
+
