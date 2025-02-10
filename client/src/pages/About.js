c9f58fc53aa9f921f38524d9820dd6ce914fcc38
aefehr <fehr.allie@gmail.com>
2025-02-10 14:57:18 -0500

    Update About page style

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -8,22 +8,39 @@ const About = () => (
     <div className="about-container">
       {/* Left Content */}
       <div className="about-content">
-        <h1 className="about-title">ABOUT</h1>
-        <div className="underline"></div>
-        <p className="about-description">
-          Hey, my name is <b>Allie Fehr</b>. I'm a senior at the University of Pennsylvania
-          studying Computer Science and Cognitive Science (concentrating in computation and cognition).
-          I’m also passionate about language learning and work part-time tutoring English to non-native speakers.
-        </p>
-        <a
-          href="/path-to-your-resume.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="resume-link"
-        >
-          <ArrowDownwardIcon className="resume-icon" />
-          resume
-        </a>
+        {/* Fixed header section */}
+        <div className="header-section">
+          <h1 className="about-title">ABOUT</h1>
+          <div className="underline"></div>
+        </div>
+        
+        {/* Scrollable content section */}
+        <div className="content-section">
+          <p className="about-description">
+            Hi, my name is <b>Allie Fehr</b>. I'm a Computer Science and Cognitive Science at the University of Pennsylvania, graduating in May 2025.
+          </p>
+          <p className="about-description">
+            My experience includes full-stack and frontend software engineering.
+            During my software engineering internship at Yahoo, I worked on the Commerce Delivery System team where I implemented
+            advanced features, including live ad previews, on the Commerce Management System using TypeScript, React, and NestJS. I also utilized Svelte & Tailwind CSS to
+            create a reusable, responsive ad template for retailer queries on Yahoo Search, estimated to generate over $10 
+            million yearly through affiliate revenue.
+          </p>
+          <p className="about-description">
+            Outside of class, I work as a peer Computer and Information Science tutor at UPenn.
+            I'm passionate about language learning and work part-time tutoring English to non-native speakers while also improving 
+            my own Spanish and Italian.
+          </p>
+          <a
+            href="/path-to-your-resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            <ArrowDownwardIcon className="resume-icon" />
+            <span className="text">resume</span>
+          </a>
+        </div>
       </div>
 
       {/* Right Image */}
