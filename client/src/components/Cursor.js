7ef45e801c5e8581d1f7467ba2d71e5e71668b12
aefehr <fehr.allie@gmail.com>
2025-02-10 20:34:48 -0500

    Add cursor effect on nav menu button

--- client/src/components/Cursor.js
+++ client/src/components/Cursor.js
@@ -83,7 +83,7 @@ const Cursor = () => {
     // Reattach hover listeners every time the route changes
     const attachHoverListeners = () => {
       const links = document.querySelectorAll(
-        "a, .link, .link span, .intro-links a, .contact-info a, .contact-info .arrow"
+        "a, .link, .link span, .intro-links a, .contact-info a, .contact-info .arrow, .nav-btn"
       );
 
       links.forEach((link) => {
